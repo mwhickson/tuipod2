353b2c54134ead626a0fd97d1c5efc877c6e3876
Matthew Hickson <matthewhickson@outlook.com>
2025-02-21 08:09:10 -0500

    added parsing of podcast RSS feed

--- tuipod2.go
+++ tuipod2.go
@@ -4,7 +4,7 @@ import (
 	"bufio"
 	"encoding/xml"
 	"fmt"
-	"io"
+	//"io"
 	"net/http"
 	"os"
 	"strings"
@@ -24,6 +24,31 @@ type Podcast struct {
 type Subscription struct {
 	Text   string `xml:"text,attr"`
 	XmlUrl string `xml:"xmlUrl,attr"`
+}
+
+type Enclosure struct {
+	Url    string `xml:"url,attr"`
+	Type   string `xml:"type,attr"`
+	Length int64  `xml:"length,attr"`
+}
+
+type Item struct {
+	Title       string    `xml:"title"`
+	Link        string    `xml:"link"`
+	Description string    `xml:"description"`
+	PubDate     string    `xml:"pubDate"`
+	Enclosure   Enclosure `xml:"enclosure"`
+}
+
+type Channel struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	Items       []Item `xml:"item"`
+}
+
+type Rss struct {
+	Channel Channel `xml:"channel"`
 }
 
 func main() {
@@ -51,6 +76,7 @@ func main() {
 	opml := make([]Subscription, 0)
 	scanner := bufio.NewScanner((file))
 
+	// TODO: get rid of entry == line expectation
 	for scanner.Scan() {
 		line := scanner.Text()
 		if strings.Contains(line, "xmlUrl") {
@@ -71,10 +97,26 @@ func main() {
 	// test network pull
 	resp, err := http.Get(opml[0].XmlUrl)
 
+	if resp != nil {
+		fmt.Println("ERROR:", err)
+	}
+
 	defer resp.Body.Close()
 
-	body, err := io.ReadAll(resp.Body)
-	body_as_string := string(body[:])
+	//body, err := io.ReadAll(resp.Body)
+	//body_as_string := string(body[:]) // TODO: tighten this up
 
-	fmt.Println("RESPONSE", body_as_string)
+	//fmt.Println("RESPONSE", body_as_string)
+
+	// TODO: parse podcast feed into our data objects
+
+	rss := Rss{}
+
+	decoder := xml.NewDecoder(resp.Body)
+	err = decoder.Decode(&rss)
+	if err != nil {
+		fmt.Println("ERROR:", err)
+	}
+
+	fmt.Println(rss)
 }
