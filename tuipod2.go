5511146f9f7d8303f9d856fd0f520301709848cb
Matthew Hickson <matthewhickson@outlook.com>
2025-02-25 22:52:22 -0500

    scrapped Bubble Tea in favour of trying out tview - let's see how that goes

--- tuipod2.go
+++ tuipod2.go
@@ -4,31 +4,19 @@ import (
 	"bufio"
 	"encoding/xml"
 	"fmt"
+
 	//"io"
-	"math"
+	//"math"
 	//"net/http"
 	"os"
 	"strings"
 
-	// REF: https://pkg.go.dev/github.com/metafates/pat/color
-	"github.com/charmbracelet/lipgloss"
-
-	"github.com/charmbracelet/bubbles/table"
-	"github.com/charmbracelet/bubbles/textinput"
-
-	tea "github.com/charmbracelet/bubbletea"
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
 )
 
 const app_name = "tuipod2"
 const statusbar_template = "STATUS"
-
-type model struct {
-	console_height int
-	console_width  int
-	search         textinput.Model
-	podcast_table  table.Model
-	episode_table  table.Model
-}
 
 type Episode struct {
 	Url   string
@@ -144,131 +132,18 @@ func main() {
 
 	//fmt.Println(rss)
 
-	// Bubble Tea UI
-	p := tea.NewProgram(initialModel(opml), tea.WithAltScreen())
-	if _, err = p.Run(); err != nil {
-		fmt.Println("ERROR running program:", err)
-		os.Exit(1)
+	app := tview.NewApplication()
+
+	frame := tview.NewFrame(tview.NewBox().SetBackgroundColor(tcell.ColorBlue)).
+		SetBorders(0, 0, 0, 0, 0, 0).
+		AddText(app_name, true, tview.AlignLeft, tcell.ColorWhite).
+		AddText(statusbar_template, false, tview.AlignLeft, tcell.ColorWhite)
+
+	// box := tview.NewBox().
+	// 	SetBorder(true).
+	// 	SetTitle(app_name)
+
+	if err := app.SetRoot(frame, true).SetFocus(frame).Run(); err != nil {
+		panic(err)
 	}
-}
-
-func initialModel(subscriptions []Subscription) model {
-	ti := textinput.New()
-
-	ti.Placeholder = "Search"
-	ti.CharLimit = 255
-
-	pt_columns := []table.Column{
-		{Title: "Podcast Title", Width: 40},
-	}
-
-	et_columns := []table.Column{
-		{Title: "Episode Title", Width: 40},
-	}
-
-	podcasts := make([]table.Row, 0)
-	for _, subscription := range subscriptions {
-		row := table.Row{subscription.Text}
-		podcasts = append(podcasts, row)
-	}
-
-	pt := table.New(
-		table.WithColumns(pt_columns),
-		table.WithRows(podcasts),
-	)
-
-	pt.SetStyles(table.DefaultStyles())
-
-	et := table.New(
-		table.WithColumns(et_columns),
-	)
-
-	et.SetStyles(table.DefaultStyles())
-
-	return model{
-		console_height: 0,
-		console_width:  0,
-		search:         ti,
-		podcast_table:  pt,
-		episode_table:  et,
-	}
-}
-
-func (m model) Init() tea.Cmd {
-	return nil
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmd tea.Cmd
-
-	switch msg := msg.(type) {
-	case tea.WindowSizeMsg:
-		m.console_height = msg.Height
-		m.console_width = msg.Width
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "esc", "ctrl+c", "q":
-			return m, tea.Quit
-		}
-	}
-
-	m.search.Width = m.console_width
-	m.search.Focus()
-
-	m.search, cmd = m.search.Update(msg)
-
-	return m, cmd
-}
-
-func (m model) View() string {
-	s := ""
-	s += RenderTitlebar(m) + "\n"
-	s += RenderSearchbar(m) + "\n"
-	s += RenderPodcastTable(m) + "\n"
-	s += RenderEpisodeTable(m) + "\n"
-	s += RenderStatusbar(m)
-
-	return s
-}
-
-func RenderTitlebar(m model) string {
-	titlebar := app_name
-	titlebar_style := lipgloss.NewStyle().
-		Width(m.console_width).
-		Foreground(lipgloss.Color("11")).
-		Background(lipgloss.Color("12"))
-
-	s := titlebar_style.Render(titlebar)
-
-	return s
-}
-
-func RenderSearchbar(m model) string {
-	return m.search.View()
-}
-
-func RenderPodcastTable(m model) string {
-	table_height := math.Ceil((float64(m.console_height) - 3.0) / 2.0)
-	m.podcast_table.SetWidth(m.console_width) // height change works, but width doesn't seem to...
-	m.podcast_table.SetHeight(int(table_height))
-	return m.podcast_table.View()
-}
-
-func RenderEpisodeTable(m model) string {
-	table_height := math.Floor((float64(m.console_height) - 3.0) / 2.0)
-	m.episode_table.SetWidth(m.console_width) // height change works, but width doesn't seem to...
-	m.episode_table.SetHeight(int(table_height))
-	return m.episode_table.View()
-}
-
-func RenderStatusbar(m model) string {
-	statusbar := statusbar_template
-	statusbar_style := lipgloss.NewStyle().
-		Width(m.console_width).
-		Foreground(lipgloss.Color("8")).
-		Background(lipgloss.Color("15"))
-
-	s := statusbar_style.Render(statusbar)
-
-	return s
 }
