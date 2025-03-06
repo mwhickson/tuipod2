078e005e037a23a2076fa266bf24010c04c5057c
Matthew Hickson <matthewhickson@outlook.com>
2025-03-05 21:52:12 -0500

    added focus shifting; references to avoid initialization cycle issues

--- tuipod2/app.go
+++ tuipod2/app.go
@@ -8,16 +8,22 @@ import (
 const app_name = "tuipod2 v0.1"
 const statusbar_template = "STATUS"
 
-func RunApplication() {
-	app := tview.NewApplication()
+var app = tview.NewApplication()
 
+var searchReference *tview.InputField
+var podcastTableReference *tview.Table
+var episodeTableReference *tview.Table
+
+func RunApplication() {
 	subscriptions := LoadSubscriptions("data/subscriptions.opml")
 
 	search := makeSearch()
 	podcast_table := makePodcastTable(subscriptions)
 	episode_table := makeEpisodeTable()
 
-	// bring it all together
+	searchReference = search
+	podcastTableReference = podcast_table
+	episodeTableReference = episode_table
 
 	flex := tview.NewFlex().
 		SetDirection(tview.FlexRow).
@@ -30,15 +36,28 @@ func RunApplication() {
 		AddText(app_name, true, tview.AlignLeft, tcell.ColorWhite).
 		AddText(statusbar_template, false, tview.AlignLeft, tcell.ColorWhite)
 
-	// TODO: figure out focus switching...
-	// if err := app.SetRoot(frame, true).SetFocus(frame).Run(); err != nil {
-	if err := app.SetRoot(frame, true).SetFocus(podcast_table).Run(); err != nil {
+	if err := app.SetRoot(frame, true).SetFocus(frame).Run(); err != nil {
 		panic(err)
 	}
 }
 
+func onSearchSubmitted(key tcell.Key) {
+	if key == tcell.KeyTab {
+		app.SetFocus(podcastTableReference)
+	}
+}
+
 func makeSearch() *tview.InputField {
-	return tview.NewInputField().SetPlaceholder("search for a podcast...")
+	search_field := tview.NewInputField().
+		SetPlaceholder("search for a podcast...").
+		SetDoneFunc(onSearchSubmitted)
+	return search_field
+}
+
+func onPodcastTableDone(key tcell.Key) {
+	if key == tcell.KeyTab {
+		app.SetFocus(episodeTableReference)
+	}
 }
 
 func makePodcastTable(subscriptions []Subscription) *tview.Table {
@@ -48,12 +67,19 @@ func makePodcastTable(subscriptions []Subscription) *tview.Table {
 	podcast_table.SetBorder(true)
 	podcast_table.SetBackgroundColor(tcell.ColorDarkBlue)
 	podcast_table.SetSelectable(true, false)
+	podcast_table.SetDoneFunc(onPodcastTableDone)
 
 	for i := range subscriptions {
 		podcast_table.SetCell(i, 0, tview.NewTableCell(subscriptions[i].Text))
 	}
 
 	return podcast_table
+}
+
+func onEpisodeTableDone(key tcell.Key) {
+	if key == tcell.KeyTab {
+		app.SetFocus(searchReference)
+	}
 }
 
 func makeEpisodeTable() *tview.Table {
@@ -63,6 +89,7 @@ func makeEpisodeTable() *tview.Table {
 	episode_table.SetBorder(true)
 	episode_table.SetBackgroundColor(tcell.ColorBlue)
 	episode_table.SetSelectable(true, false)
+	episode_table.SetDoneFunc(onEpisodeTableDone)
 
 	return episode_table
 }
